0e636b32947344cf7114381fc6871913344f1725
yanlu <caoqinmiao@gmail.com>
2016-08-22 21:39:12 +0800

    add log fragment

---
+++ example/src/main/java/com/github/captain_miao/android/bluetoothletutorial/DeviceCommunicateActivity.java
@@ -0,0 +1,112 @@
+package com.github.captain_miao.android.bluetoothletutorial;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+
+import com.flyco.tablayout.SlidingTabLayout;
+import com.flyco.tablayout.listener.OnTabSelectListener;
+import com.github.captain_miao.android.bluetoothletutorial.constant.AppConstants;
+import com.github.captain_miao.android.bluetoothletutorial.fragment.BleLogFragment;
+import com.github.captain_miao.android.bluetoothletutorial.model.BleDevice;
+import com.github.captain_miao.android.supportsdk.BaseActivity;
+
+import java.util.ArrayList;
+
+/**
+ * @author YanLu
+ * @since 16/8/22
+ */
+public class DeviceCommunicateActivity extends BaseActivity implements OnTabSelectListener {
+    private static final String TAG = "DeviceCommunicate";
+
+    private BleDevice mDevice;
+    private String mDeviceName;
+    private String mDeviceAddress;
+
+
+    private Context mContext = this;
+    private ArrayList<Fragment> mFragments = new ArrayList<>();
+    private final String[] mTitles = {
+            "Device", "Data", "Log"
+    };
+    private DevicePagerAdapter mAdapter;
+    @Override
+    public void init(Bundle savedInstanceState) {
+        setContentView(R.layout.act_device_commnunicate);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+
+        mDevice = (BleDevice) getIntent().getSerializableExtra(AppConstants.KEY_BLE_DEVICE);
+        mDeviceName = TextUtils.isEmpty(mDevice.name) ? mDevice.address : mDevice.name;
+        mDeviceAddress = mDevice.address;
+
+        setTitle(mDeviceName);
+
+
+        for (String title : mTitles) {
+            mFragments.add(BleLogFragment.getInstance(title));
+        }
+
+
+        ViewPager vp = (ViewPager) findViewById(R.id.vp);
+        mAdapter = new DevicePagerAdapter(getSupportFragmentManager());
+        vp.setAdapter(mAdapter);
+
+        SlidingTabLayout tabLayout_1 = (SlidingTabLayout) findViewById(R.id.tl_1);
+        SlidingTabLayout tabLayout_2 = (SlidingTabLayout) findViewById(R.id.tl_2);
+        SlidingTabLayout tabLayout_3 = (SlidingTabLayout) findViewById(R.id.tl_3);
+
+
+        tabLayout_1.setViewPager(vp);
+        tabLayout_2.setViewPager(vp);
+        tabLayout_3.setViewPager(vp);
+
+        tabLayout_1.setOnTabSelectListener(this);
+        tabLayout_2.setOnTabSelectListener(this);
+        tabLayout_3.setOnTabSelectListener(this);
+
+        vp.setCurrentItem(0);
+    }
+
+    @Override
+    public void onTabSelect(int position) {
+
+    }
+
+    @Override
+    public void onTabReselect(int position) {
+
+    }
+
+
+    private class DevicePagerAdapter extends FragmentPagerAdapter {
+        public DevicePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mTitles[position];
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+    }
+}
---
+++ example/src/main/java/com/github/captain_miao/android/bluetoothletutorial/fragment/BleLogFragment.java
@@ -0,0 +1,37 @@
+package com.github.captain_miao.android.bluetoothletutorial.fragment;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.github.captain_miao.android.bluetoothletutorial.R;
+
+
+@SuppressLint("ValidFragment")
+public class BleLogFragment extends Fragment {
+    private String mTitle;
+
+    public static BleLogFragment getInstance(String title) {
+        BleLogFragment sf = new BleLogFragment();
+        sf.mTitle = title;
+        return sf;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.frg_ble_log, null);
+        TextView card_title_tv = (TextView) v.findViewById(R.id.card_title_tv);
+        card_title_tv.setText(mTitle);
+
+        return v;
+    }
+}
